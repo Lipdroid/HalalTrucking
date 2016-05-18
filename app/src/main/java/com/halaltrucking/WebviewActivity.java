21bb49d70e8b7c05ef1886a0da110f953f7ca539
WebHawks IT <lipuhossain67@gmail.com>
2016-05-18 14:07:24 +0600

    added about section,distance sorting and change all categories

--- app/src/main/java/com/halaltrucking/WebviewActivity.java
+++ app/src/main/java/com/halaltrucking/WebviewActivity.java
@@ -1,6 +1,7 @@
 package com.halaltrucking;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +26,7 @@ public class WebviewActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_webview);
         String url = getIntent().getExtras().getString("url");
 
